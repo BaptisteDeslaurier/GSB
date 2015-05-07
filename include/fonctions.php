409c6b92757e531cf9116c0f3e0a944ec61bbe98
Baptiste Deslaurier <baptiste.deslaurier@bts-malraux.net>
2015-05-07 09:18:07 +0200

    Mise en fonction de la page saisie_frais.php et documentation technique des fonctions

--- include/fonctions.php
+++ include/fonctions.php
@@ -1,11 +1,17 @@
 <?php
 
+// Fonctions de la page affiche_frais.php
+
+/**
+ * Fontion qui recupère les différents dates (sans doublons)
+ * @param varchar $connexion
+ */
 function listeDeroulanteMoisAnnee($connexion) {
     //Recherche et récupềre les différents dates (sans doublons) des toutes les fiches de frais
-    $resultatRecherche = $connexion->query('SELECT DISTINCT mois FROM fichefrais ORDER BY mois DESC');
+    $listeMois = $connexion->query('SELECT DISTINCT mois FROM fichefrais ORDER BY mois DESC');
                 
     // Pour chaque date des dates récupérées
-    while($Mois = $resultatRecherche->fetch()) {
+    while($Mois = $listeMois->fetch()) {
         // Création d'un tableau des mois
         $tabMois = array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
         // Segmentation de la chaine date pour en extraire le mois 
@@ -19,9 +25,15 @@ function listeDeroulanteMoisAnnee($connexion) {
         <?php
     }
     // Ferme le curseur, permettant à la requête d'être de nouveau exécutée
-    $resultatRecherche->closeCursor();
+    $listeMois->closeCursor();
 }
 
+/**
+ * Fontion qui va afficher la phrase montrant l'état de la fiche
+ * @param varchar $connexion
+ * @param varchar $mois
+ * @param integer $id
+ */
 function phraseEtatFiche($connexion, $mois, $id) {
     // Création d'une variable de session contenant le mois pour le cas où l'on veut créer le PDF
     $_SESSION['mois']=$mois;
@@ -33,56 +45,70 @@ function phraseEtatFiche($connexion, $mois, $id) {
     $lAnnee = substr($_POST['mois'], 0,4);
     
     // Récupération de l'état de la fiche (création, remboursé ...) et de sa date de der,ière modifiaction
-    $resultatEtat = $connexion->query('SELECT libelle, dateModif
+    $resultatEtatFiche = $connexion->query('SELECT libelle, dateModif
                                     FROM etat E
                                     INNER JOIN fichefrais FF ON E.id = FF.idEtat
-                                    WHERE FF.idVisiteur = "'.$_SESSION['id'].'"
+                                    WHERE FF.idVisiteur = "'.$id.'"
                                     AND FF.mois = "'.$_POST['mois'].'"');
 
-    while($typeEtat = $resultatEtat->fetch()) {
+    while($typeEtat = $resultatEtatFiche->fetch()) {
         $etat = $typeEtat['libelle'];
         $date = $typeEtat['dateModif'];
     }
-    $resultatEtat->closeCursor();
+    $resultatEtatFiche->closeCursor();
     
     echo "Fiche de frais du mois de ".$leMois." ".$lAnnee." : ".$etat. " depuis le ".$date;
 }
 
+/**
+ * Fontion qui va afficher le montant de la fiche de frais
+ * @param varchar $connexion
+ * @param integer $id
+ */
 function montantFiche($connexion, $id) {
-    $resultat = $connexion->query('SELECT montantValide 
+    $resultatMontantFiche = $connexion->query('SELECT montantValide 
                                  FROM fichefrais 
-                                 WHERE idVisiteur="'.$_SESSION['id'].'" 
+                                 WHERE idVisiteur="'.$id.'" 
                                  AND mois = "'.$_POST['mois'].'"');
 
-    if ($ligne = $resultat->fetch()) {
+    if ($ligne = $resultatMontantFiche->fetch()) {
         $montant = $ligne['montantValide'];
         echo '<br/><br/>';
         echo 'Montant validé : '.$montant.'';
     }
+    $resultatMontantFiche->closeCursor();
 }
 
+/**
+ * Fontion qui va afficher les différents éléments non hors forfait de la fiche sélectionnée
+ * @param varchar $connexion
+ * @param integer $id
+ */
 function ligneTableauForfait($connexion, $id) {
-    $resultat2 = $connexion->query('SELECT quantite
+    $resultatElementForfait = $connexion->query('SELECT quantite
                                   FROM lignefraisforfait 
-                                  WHERE idVisiteur="'.$_SESSION['id'].'" 
+                                  WHERE idVisiteur="'.$id.'" 
                                   AND mois = "'.$_POST['mois'].'"');
 
-
-    while($ligne = $resultat2->fetch()) {
+    while($ligne = $resultatElementForfait->fetch()) {
         $idfrais = $ligne['quantite'];     
         echo  "<td width='25%' align='center'>".$idfrais."</td>";         
-     }
-     
-     $resultat2->closeCursor();
+    }
+    $resultatElementForfait->closeCursor();
 }
 
-function ligneTableauHorsForfait($connexion) {
-    $resultat3 = $connexion->query('SELECT DATE, montant, libelle 
+/**
+ * Fontion qui va afficher les différents éléments hors forfait de la fiche sélectionnée
+ * @param varchar $connexion
+ * @param integer $id
+ */
+function ligneTableauHorsForfait($connexion, $id) {
+    $resultatElementHorsForfait = $connexion->query('SELECT DATE, montant, libelle 
                                   FROM lignefraishorsforfait 
                                   WHERE mois="'.$_POST['mois'].'" 
-                                  AND idVisiteur="'.$_SESSION['id'].'" order by mois desc');
+                                  AND idVisiteur="'.$id.'" order by mois desc');
     
-    while($ligne=$resultat3->fetch()) {
+    while($ligne = $resultatElementHorsForfait->fetch()) {
         $date = $ligne['DATE'];
         $montant = $ligne['montant'];
         $libelle = $ligne['libelle'];
@@ -94,8 +120,136 @@ function ligneTableauHorsForfait($connexion) {
              <td width='20%' align='center'>$montant</td>
          </tr>";
     }
-    
-    $resultat3->closeCursor();
+    $resultatElementHorsForfait->closeCursor();
 }
 
+// Fonction de la page maj_frais_forfait.php
+
+/**
+ * Fontion qui va mettre à jour les éléments non hors forfait
+ * @param varchar $connexion
+ */
+function updateFraisForfait($connexion) {
+    $connexion->exec ('update lignefraisforfait set quantite = quantite + '.$_POST['etape'].' where idFraisForfait = "ETP" and idVisiteur = "'.$_SESSION['id'].'" and mois = "'.$_SESSION['annee_mois']. '"');
+    $connexion->exec ('update lignefraisforfait set quantite = quantite + '.$_POST['km'].' where idFraisForfait = "KM" and idVisiteur = "'.$_SESSION['id'].'" and mois = "'.$_SESSION['annee_mois']. '"');
+    $connexion->exec ('update lignefraisforfait set quantite = quantite + '.$_POST['nuit'].' where idFraisForfait = "NUI" and idVisiteur = "'.$_SESSION['id'].'" and mois = "'.$_SESSION['annee_mois']. '"');
+    $connexion->exec ('update lignefraisforfait set quantite = quantite + '.$_POST['repas'].' where idFraisForfait = "REP" and idVisiteur = "'.$_SESSION['id'].'" and mois = "'.$_SESSION['annee_mois']. '"');
+
+    header('location: saisie_frais.php');
+}
+
+// Fonction de la page maj_frais_hors_forfait.php
+
+/**
+ * Fontion qui va mettre à jour les éléments hors forfait
+ * @param varchar $connexion
+ */
+function updateFraisHorsForfait($connexion) {
+    $idMaxHF = $connexion->query('select MAX(id) as idMax from lignefraishorsforfait');
+    $id = $idMaxHF->fetch();
+    $idMax = ($id['idMax']+1);
+
+    $connexion->exec ('insert into lignefraishorsforfait values('.$idMax.', "'.$_SESSION['id'].'", "'.$_SESSION['annee_mois'].'", "'.$_POST['libelle'].'", "' .$_POST['date'].'", ' .$_POST['montant']. ')');
+
+    header('location: saisie_frais.php');
+}
+
+// Fonction de la page saisie_frais.php
+
+/**
+ * Fonction de création de la fiche de frais
+ * @param varchar $connexion
+ * @param integer $id
+ */
+function creationFicheFrais($connexion, $id){
+    // Initialisation du jour et du mois
+    $jour = date("d");
+    $annee = date('Y');
+    if ($jour < 10)// test pour savoir sur quel mois on est.
+    {
+        if (date('m') == 1)// test du mois d'une année inférieur 
+        {
+            $mois = 12;//on renvoit vers le mois d'après (décembre 12)
+            $annee = $annee - 1;//on renvoit vers l'année précédente
+        }
+        else
+        {
+            $mois = date('m') - 1;
+        }
+    }
+     else 
+    {
+         $mois = date('m');
+    }
+    // On vérifie si une fiche de frais existe pour ce mois là
+    $_SESSION['annee_mois'] = $annee.$mois;
+    $sql = "select * from fichefrais where idVisiteur = '" .$id."' and mois='".$_SESSION['annee_mois']."'";
+    $resultatFicheDuMois = $connexion->query($sql);
+    if (!$ligne = $resultatFicheDuMois->fetch())
+    {
+    //pas de fiche de frais pour ce mois là on la crée avec les lignes frais forfait correpondante
+        $connexion->exec("insert into fichefrais values ('".$id."', '".$_SESSION['annee_mois']."', 0, 0, NULL, 'CR')");
+        $connexion->exec("insert into lignefraisforfait select '".$id."', '".$_SESSION['annee_mois']."', id, 0 from fraisforfait");
+    }
+    $resultatFicheDuMois->closeCursor();
+}
+/**
+ * Fontion qui recupère la quantite de forfais étape, frais kilometrique, nuitée, et repas restaurant
+ * @param varchar $connexion
+ * @param integer $id
+ * $resultatForfaitEtape int(11) ETP
+ * $forfaitEtape = $resultatForfaitEtape
+ * $resultatFraisKm int(11) KM
+ * $fraisKm = $resultatFraisKm
+ * $resultatNuitee int(11) NUI
+ * $nuitee = $resultatNuitee
+ * $resultatRepas int(11) REP
+ * $repas = $resultatRepas
+ */
+function recuperationElementsForfaitises($connexion, $id){
+    // Recupération des forfaits étapes
+    $resultatForfaitEtape = $connexion->query('SELECT quantite FROM lignefraisforfait WHERE idVisiteur = "' .$id. '" AND idFraisForfait = "ETP" AND mois = "' .$_SESSION['annee_mois']. '"');
+    // Recupère le resultat optenue par la requéte
+    $forfaitEtape = $resultatForfaitEtape->fetch();
+    // Recupération des frais KM
+    $resultatFraisKm = $connexion->query('SELECT quantite FROM lignefraisforfait WHERE idVisiteur = "' .$id. '" AND idFraisForfait = "KM" AND mois = "' .$_SESSION['annee_mois']. '"');
+    $fraisKm = $resultatFraisKm->fetch();
+    // Recupération des frais nuitee
+    $resultatNuitee = $connexion->query('SELECT quantite FROM lignefraisforfait WHERE idVisiteur = "' .$id. '" AND idFraisForfait = "NUI" AND mois = "' .$_SESSION['annee_mois']. '"');
+    $nuitee = $resultatNuitee->fetch();
+    // Recupération des frais repas
+    $resultatRepas = $connexion->query('SELECT quantite FROM lignefraisforfait WHERE idVisiteur = "' .$id. '" AND idFraisForfait = "REP" AND mois = "' .$_SESSION['annee_mois']. '"');
+    $repas = $resultatRepas->fetch();
+    ?>
+    <h3>Tableau recapitulatif des éléments forfaitisé</h3>
+            <tr>
+                <td><?php echo $forfaitEtape['quantite']; ?></td>
+                <td><?php echo $fraisKm['quantite']; ?></td>
+                <td><?php echo $nuitee['quantite']; ?></td>
+                <td><?php echo $repas['quantite']; ?></td>
+            </tr>
+        </table>
+    <?php
+    }
+    
+    /**
+     * Fontion qui recupère les éléments hors forfaits
+     * @param varchar $connexion
+     * @param integer $id
+     * $resultatHorsForfait -> recupère le libelle, la date et le montant dans la table lignefraishorsforfait quand l'id visiteur = a l'id de l'utilisateur connecté et que le mois = la valeur du mois initialisé dans creationFicheFrais
+     */
+    function recuperationElementsHorsForfait($connexion, $id){
+        $resultatHorsForfait = $connexion->query('select libelle, date, montant from lignefraishorsforfait where idVisiteur = "' .$id. '" and mois = ' .$_SESSION['annee_mois']);
+        while($horsForfait = $resultatHorsForfait->fetch())
+        {
+        ?>
+                    <tr>
+                        <td><?php echo date("d/m/Y", strtotime($horsForfait['date'])); ?></td>
+                        <td><?php echo $horsForfait['libelle']; ?></td>
+                        <td><?php echo $horsForfait['montant']; ?></td>
+                    </tr>
+    <?php
+        }
+        $resultatHorsForfait->closeCursor();
+    }
 ?>
--- maj_frais_forfait.php
+++ maj_frais_forfait.php
@@ -2,12 +2,8 @@
 session_start();
 
 include './include/connexion_bd.php';
+include './include/fonctions.php';
 
-$connexion->exec ('update lignefraisforfait set quantite = quantite + '.$_POST['etape'].' where idFraisForfait = "ETP" and idVisiteur = "'.$_SESSION['id'].'" and mois = "'.$_SESSION['annee_mois']. '"');
-$connexion->exec ('update lignefraisforfait set quantite = quantite + '.$_POST['km'].' where idFraisForfait = "KM" and idVisiteur = "'.$_SESSION['id'].'" and mois = "'.$_SESSION['annee_mois']. '"');
-$connexion->exec ('update lignefraisforfait set quantite = quantite + '.$_POST['nuit'].' where idFraisForfait = "NUI" and idVisiteur = "'.$_SESSION['id'].'" and mois = "'.$_SESSION['annee_mois']. '"');
-$connexion->exec ('update lignefraisforfait set quantite = quantite + '.$_POST['repas'].' where idFraisForfait = "REP" and idVisiteur = "'.$_SESSION['id'].'" and mois = "'.$_SESSION['annee_mois']. '"');
-
-header('location: saisie_frais.php');
+updateFraisForfait($connexion);
 
 ?>
